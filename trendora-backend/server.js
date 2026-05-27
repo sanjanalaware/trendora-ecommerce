44c752aec9f794db9432b7bf02a55c0435a68933
sanjanalaware <lawaresanjana@gmail.com>
2026-05-28 02:57:38 +0530

    Updated CORS for production

--- trendora-backend/server.js
+++ trendora-backend/server.js
@@ -11,7 +11,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://trendora-ecommerce-teal.vercel.app/",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
