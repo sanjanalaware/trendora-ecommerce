26627520366890b9bb105d2accfea8d5ceeac5fb
sanjanalaware <lawaresanjana@gmail.com>
2026-05-28 02:53:07 +0530

    backend hosted url added

--- trendora-frontend/src/services/api.js
+++ trendora-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://trendora-backend-zy6y.onrender.com/api",
 });
 
 export default API;
