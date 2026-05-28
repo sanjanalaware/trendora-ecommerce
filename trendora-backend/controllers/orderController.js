e08e55f8b2d906ce984754cd74d333fefabc7373
sanjanalaware <lawaresanjana@gmail.com>
2026-05-29 00:34:28 +0530

    myorder api added

--- trendora-backend/controllers/orderController.js
+++ trendora-backend/controllers/orderController.js
@@ -41,3 +41,17 @@ export const updateOrderStatus = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getMyOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({
+      user: req.user._id,
+    }).sort({ createdAt: -1 });
+
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- trendora-backend/routes/orderRoutes.js
+++ trendora-backend/routes/orderRoutes.js
@@ -3,6 +3,7 @@ import {
   createOrder,
   getAllOrders,
   updateOrderStatus,
+  getMyOrders,
 } from "../controllers/orderController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
@@ -11,7 +12,7 @@ const router = express.Router();
 
 // USER
 router.post("/", protect, createOrder);
-
+router.get("/my-orders", protect, getMyOrders);
 // ADMIN
 router.get("/", protect, getAllOrders);
 router.put("/:id", protect, updateOrderStatus);
