c7d6939bafdb1d1a727be6def8a1e553c49df40a
sanjanalaware <lawaresanjana@gmail.com>
2026-05-28 23:15:37 +0530

    order upated api

--- trendora-backend/controllers/orderController.js
+++ trendora-backend/controllers/orderController.js
@@ -24,3 +24,20 @@ export const getAllOrders = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+export const updateOrderStatus = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    order.status = req.body.status || order.status;
+
+    await order.save();
+
+    res.json(order);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
